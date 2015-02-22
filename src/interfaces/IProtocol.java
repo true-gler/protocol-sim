ba40dd8e574965703163a4401e802b580b38d107
Thomas <Thomas@thinkpad.home>
2015-02-22 20:46:48 +0100

    order

--- src/interfaces/IProtocol.java
+++ src/interfaces/IProtocol.java
@@ -5,10 +5,10 @@ import model.Paket;
 import event.Event;
 
 public interface IProtocol {
-	
-	public void executePreSimulation(Object... object);
+		
 	public void executeTX(Object... object);
 	public void executeRX(Object... object);
+	public void executePreSimulation(Object... object);
 	public void executeFinished(Object... object);
 	public void executePostSimulation(Object... object);
 }
